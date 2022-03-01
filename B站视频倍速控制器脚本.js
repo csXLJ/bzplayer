e8ccce578ba7c4f8a59ab5436e6499f0a1b634b2
csXLJ <40972201+csXLJ@users.noreply.github.com>
2022-03-01 17:34:29 +0800

    兼容所有视频倍速以及倍速展示样式

--- B站视频倍速控制器脚本.js
+++ B站视频倍速控制器脚本.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         B站视频倍速控制器脚本
 // @namespace    https://github.com/csXLJ/bzplayer/
-// @version      1.0
+// @version      2.0
 // @description  调整B站视频播放速度
 // @author       csXLJ
+// @icon         https://www.bilibili.com/favicon.ico?v=1
 // @include      https://www.bilibili.com/*
 // @match        https://www.bilibili.com/video/*
 // @grant        none
+// @license      GPL
 // ==/UserScript==
 
 var controller = (function () {
@@ -15,7 +17,6 @@ var controller = (function () {
     function keyd(e) {
         var whichVideo = document.querySelector('bwp-video') != null ? document.querySelector('bwp-video') : document.querySelector('video');
         speed = whichVideo.playbackRate;
-        var video = document.querySelector('.bilibili-player-video-inner');
         if (e.key == "+") {
             if (speed == 16) {
                 speed = 16;
@@ -31,8 +32,8 @@ var controller = (function () {
                 speed = roundFun(speed, 1);
             }
         }
-        var str = speed + "x";
-        addAndDelTil(video, "div", str);
+        var str = "播放速度："+speed + "x";
+        addAndDelTil(whichVideo, "div", str);
         whichVideo.playbackRate = speed;
     }
 
@@ -45,20 +46,26 @@ var controller = (function () {
 
     function addAndDelTil(element, tab, str) {
         if (timeId != null) {
-            $(element).children().first().remove();
+            $(element).prev().remove();
             clearTimeout(timeId);
         }
-        // 创建节点
         var para = document.createElement(tab);
-        // 创建文本节点
         var node = document.createTextNode(str);
         para.appendChild(node);
         para.style.position = "absolute";
+        para.style.color = "white";
+        //para.style.textShadow = "2px 2px 5px #00a1d6";
+        para.style.fontWeight = "bold";
+        para.style.borderRadius = "15px";
+        para.style.border = "2px solid #00a1d6";
+        para.style.backgroundColor = "#00a1d6";
+        para.style.padding = "1px";
+        para.style.opacity = "0.5";
+        para.style.fontFamily = "-apple-system,BlinkMacSystemFont,Helvetica Neue,Helvetica,Arial,PingFang SC,Hiragino Sans GB,Microsoft YaHei,sans-serif";
         para.style.left = "1%";
         para.style.top = "1%";
-        para.style.zIndex = 100;
-        // 把p节点添加到
-        element.prepend(para);
+        para.style.zIndex = 99999;
+        element.before(para);
         timeId = setTimeout(function () {
             para.remove();
             clearTimeout(timeId);
